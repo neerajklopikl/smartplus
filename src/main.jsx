1a73ee0d4dacba4e6ce7f6fcd13d5a7b86ce1e66
neerajklopikl <neerajvaishalishahg@gmail.com>
2025-09-04 17:28:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { HashRouter } from 'react-router-dom'; // Changed from BrowserRouter
-import { CartProvider } from './CartContext.jsx';
+import { HashRouter } from 'react-router-dom';
+import { CartProvider } from './CartContext.jsx'; // 1. Import CartProvider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Using HashRouter to work with static hosting like GitHub Pages */}
     <HashRouter>
       <CartProvider>
         <App />
