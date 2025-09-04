c35794bd29a152a1e63118807d8838e75e9a6759
neerajklopikl <neerajvaishalishahg@gmail.com>
2025-09-04 17:27:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// This configuration sets the base path for the deployed app.
+// It should match your repository name on GitHub.
 export default defineConfig({
   plugins: [react()],
   base: "/smartplus/",
-});
+})
