46c533bb81a58b9cb7840ff7f048e87637200518
neerajklopikl <neerajvaishalishahg@gmail.com>
2025-09-04 17:17:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './CartContext.jsx'; // 1. Import CartProvider
+import { HashRouter } from 'react-router-dom'; // Changed from BrowserRouter
+import { CartProvider } from './CartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      {/* 2. Wrap App with CartProvider */}
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
